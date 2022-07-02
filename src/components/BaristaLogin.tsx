563d93a7f132a291bf6650e2bfa1f95984819c9a
dwells2024 <davidwells2024@u.northwestern.edu>
2022-07-02 12:28:47 -0500

    added barista login elements

--- src/components/BaristaLogin.tsx
+++ src/components/BaristaLogin.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function BaristaLogin() {
+  const [barista, setBarista] = useState('');
+
+  const login = () => {
+    let name = (document.getElementById('baristaName') as HTMLInputElement);
+    console.log(name!.value);
+  }
+
+  const pass = () => {};
+
   return (
-    <div>BaristaLogin</div>
+    <div>
+      <label htmlFor='baristaName'>Barista: </label>
+      <input id='baristaName' name='baristaName' defaultValue={barista} type='text'/>
+      <button name='login' onClick={login}>Login</button>
+      <button name='logout'>Logout</button>
+      {/* <p>{document.getElementById('baristaName').value}</p> */}
+    </div>
   )
 }
 
